2f54fafdc94fc59529fa9840b254b0cea02a978b
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2023-12-18 22:09:09 -0300

    feat: :heavy_plus_sign: add framer-motion

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -2,6 +2,6 @@ export const routes = {
   home: { name: "home", path: "/" },
   projects: { name: "projects", path: "/projects" },
   about: { name: "about", path: "/about" },
-  skills: { name: "skills", path: "/skills" },
+  // skills: { name: "skills", path: "/skills" },
   contact: { name: "contact", path: "/contact" },
 };
